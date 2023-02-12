8ae483e3e1c4fda38ee4efbe30a9afcded69ca79
Joshua Eseigbe <joshsupersport@gmail.com>
2023-02-12 18:36:42 +0100

    fix:changes to home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,15 +83,15 @@
             <!-- Blog preview section-->
             <section class="py-5">
                 <div class="container px-5 my-5">
-                    <div class="row gx-5 justify-content-center">
+                    <!-- <div class="row gx-5 justify-content-center">
                         <div class="col-lg-8 col-xl-6">
                             <div class="text-center">
                                 <h2 class="fw-bolder">From our blog</h2>
                                 <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
                             </div>
                         </div>
-                    </div>
-                    <div class="row gx-5">
+                    </div> -->
+                    <!-- <div class="row gx-5">
                         <div class="col-lg-4 mb-5">
                             <div class="card h-100 shadow border-0">
                                 <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d" alt="..." />
@@ -155,7 +155,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <!-- Call to action-->
                     <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
                         <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
